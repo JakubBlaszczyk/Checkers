b742382bb2a37abc04f67ff3b84e71031967350b
ronbun <82181753+ronbun@users.noreply.github.com>
2021-10-13 20:28:15 +0200

    Change to custom exceptions

--- src/main/java/com/pk/server/ServerController.java
+++ src/main/java/com/pk/server/ServerController.java
@@ -1,16 +1,19 @@
 package com.pk.server;
 
 import java.io.IOException;
+import java.nio.channels.ClosedChannelException;
 import java.security.InvalidAlgorithmParameterException;
 import java.util.List;
 
+import com.pk.server.exceptions.ChatMsgRejected;
+import com.pk.server.exceptions.MoveRejected;
 import com.pk.server.models.Invite;
 import com.pk.server.models.Move;
 import com.pk.server.models.Player;
 
 public interface ServerController {
-  public void move(Move move) throws IOException;
-  public void chatSendMsg(String msg) throws IOException;
+  public void move(Move move) throws ClosedChannelException, IOException, MoveRejected;
+  public void chatSendMsg(String msg) throws ClosedChannelException, IOException, ChatMsgRejected ;
   public List<Player> findPlayers();
   public GameSession invite(Invite invite) throws InvalidAlgorithmParameterException, IOException;
   public GameSession acceptInvitation(Invite invite) throws IOException, InvalidAlgorithmParameterException;
