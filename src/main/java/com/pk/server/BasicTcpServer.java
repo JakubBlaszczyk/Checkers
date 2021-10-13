964a366d82e06287078eee0ccaa0533f5a96f192
ronbun <82181753+ronbun@users.noreply.github.com>
2021-10-13 15:52:37 +0200

    Revert to one tcp class, add cleanup

--- src/main/java/com/pk/server/BasicTcpServer.java
+++ src/main/java/com/pk/server/BasicTcpServer.java
@@ -14,6 +14,7 @@ import java.util.concurrent.BlockingQueue;
 
 import com.pk.server.models.Invite;
 
+import lombok.Cleanup;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -27,7 +28,6 @@ public class BasicTcpServer implements TcpServer {
   private @Getter @Setter @NonNull BlockingQueue<Invite> bQueue;
   private Selector selector;
   private ServerSocketChannel serverSocketChannel;
-  private TcpHandler tcpHandler;
 
   public BasicTcpServer(BlockingQueue<Invite> bQueue, String ip, Integer port) throws IOException {
     this.bQueue = bQueue;
@@ -36,7 +36,11 @@ public class BasicTcpServer implements TcpServer {
     serverSocketChannel.configureBlocking(false);
     serverSocketChannel.bind(new InetSocketAddress(ip, port));
     serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-    tcpHandler = new TcpHandler();
+  }
+
+  public void cleanup() throws IOException {
+    serverSocketChannel.close();
+    selector.close();
   }
 
   /**
@@ -61,7 +65,7 @@ public class BasicTcpServer implements TcpServer {
             log.info("Connection Accepted: " + sc.getLocalAddress());
           }
           if (key.isReadable()) {
-            tcpHandler.readMsg(key, bQueue);
+            readInvite(key, bQueue);
           }
         }
       }
@@ -86,20 +90,35 @@ public class BasicTcpServer implements TcpServer {
       sc.configureBlocking(true);
     }
     // checkers:invitationOk
-    tcpHandler.sendPacket(sc, ByteBuffer.wrap("checkers:invitationAsk".getBytes()));
+    sc.write(ByteBuffer.wrap("checkers:invitationAsk".getBytes()));
     ByteBuffer buf = ByteBuffer.wrap(new byte[100]);
-    tcpHandler.receivePacket(sc, buf);
-    String msg = new String(buf.array());
+    sc.read(buf);
+    String msg = new String(buf.array(), 0, getBytesLen(buf));
     if (msg.equals("checkers:invitationOk")) {
       log.info("Invitation accepted");
       return new BasicGameSession(sc);
-      // FIXME add exceptions
     } else if (msg.equals("checkers:invitationRejected")) {
-      log.info("Invitation rejected");
-      return null;
+      throw new InvalidAlgorithmParameterException("Invitation rejected");
     } else {
-      log.info("Invalid response: " + msg);
-      return null;
+      throw new InvalidAlgorithmParameterException("Invalid response: " + msg);
+    }
+  }
+
+  public static int getBytesLen(ByteBuffer bb) {
+    int len = 0;
+    try {
+      byte[] arr = bb.array();
+      for (int i = 0;; ++i) {
+        int x = arr[i];
+        log.debug("ARR: " + x);
+        if (x != 0) {
+          len += 1;
+          continue;
+        }
+        return len;
+      }
+    } catch (Exception e) {
+      return len;
     }
   }
 
@@ -119,7 +138,63 @@ public class BasicTcpServer implements TcpServer {
       sc.configureBlocking(true);
     }
     // checkers:invitationOk
-    tcpHandler.sendPacket(sc, ByteBuffer.wrap("checkers:invitationOk".getBytes()));
+    sc.write(ByteBuffer.wrap("checkers:invitationOk".getBytes()));
     return new BasicGameSession(sc);
+  }
+
+  public void readInvite(SelectionKey key, BlockingQueue<Invite> bQueue) throws IOException {
+    SocketChannel sc = (SocketChannel) key.channel();
+    ByteBuffer bb = ByteBuffer.allocate(1024);
+    int len = sc.read(bb);
+    if (len <= 0) {
+      sc.close();
+      log.info("Connection closed");
+      return;
+    }
+    String result = new String(bb.array(), 0, len);
+    log.info("Message received: " + result + " Message length: " + result.length());
+    if (addNewInvite(result, sc, bQueue)) {
+      key.cancel();
+    }
+  }
+
+  /**
+   * Parse network message if correct type, extract nickname and profilePicture
+   * encoded in base64 and add it to invite queue
+   * 
+   * @param msg Message received from interface
+   * @param sc  Socket from which message arrived
+   * @return Whether message was indeed invitation
+   */
+  private boolean addNewInvite(String msg, SocketChannel sc, BlockingQueue<Invite> bQueue) {
+    try {
+      if (!msg.substring(0, 9).equals("checkers:")) {
+        log.warn("Invalid substring: ", msg.substring(0, 9));
+        return false;
+      }
+      // checkers:invitationAsk nick profileImg
+      msg = msg.substring(9);
+      if (!msg.substring(0, 14).equals("invitationAsk ")) {
+        log.info("Invalid substring: ", msg.substring(0, 14));
+        return false;
+      }
+      String[] items = msg.substring(14).split(" ");
+      if (items.length != 2) {
+        log.warn("Invalid size: " + items.length + ", items: " + items[0]);
+        return false;
+      }
+      log.info(String.valueOf(items.length));
+      for (String item : items) {
+        log.info(item);
+      }
+      // FIXME same info stored 2 times
+      Invite invite = new Invite(sc.socket().getInetAddress(), items[0], items[1], sc);
+      log.info(invite.toString());
+      bQueue.add(invite);
+      return true;
+    } catch (IndexOutOfBoundsException e) {
+      log.warn("Exception: ", e);
+      return false;
+    }
   }
 }
