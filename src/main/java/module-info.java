4f8c21da3842766c817e56100fb5b53ddbebffe6
ronbun <82181753+ronbun@users.noreply.github.com>
2021-10-18 10:41:07 +0200

    Export all classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 module com.pk {
     requires transitive javafx.controls;
     requires javafx.fxml;
@@ -5,8 +8,13 @@ module com.pk {
     requires org.slf4j;
 
     opens com.pk.server;
-    opens com.pk to javafx.fxml;
-    opens com.pk.frontend.menu to javafx.fxml;
+    opens com.pk;
+    opens com.pk.frontend.menu;
     exports com.pk;
-    exports com.pk.frontend.menu to javafx.fxml;
+    exports com.pk.logic;
+    exports com.pk.logic.exceptions;
+    exports com.pk.server;
+    exports com.pk.server.exceptions;
+    exports com.pk.server.models;
+    exports com.pk.frontend.menu;
 }
