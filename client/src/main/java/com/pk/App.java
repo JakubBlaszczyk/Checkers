391adc0ee6ca6fa4d828b17bcbcbf431d0c34f35
gabafior <egzystencjalna.pustka@gmail.com>
2021-12-08 22:35:17 +0100

    add board and delete unnecessary files

--- client/src/main/java/com/pk/App.java
+++ client/src/main/java/com/pk/App.java
@@ -1,36 +1,70 @@
-package com.pk;
+package com.pk.checkers;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-/**
- * Hello world!
- *
- */
 public class App extends Application {
 
-  public Stage stage;
-  public ResourceBundle translations;
-  public Locale locale;
+    public static final int TILE_SIZE = 100;
+    public static final int WIDTH = 8;
+    public static final int HEIGHT = 8;
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    stage = primaryStage;
-    locale = new Locale("pl_PL");
-    translations = ResourceBundle.getBundle("translations", locale);
-    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainMenuView.fxml"), translations);
-    primaryStage.setTitle("Checkers");
-    primaryStage.setScene(new Scene(root, 800, 800));
-    primaryStage.show();
-  }
+    private Tile[][] board = new Tile[WIDTH][HEIGHT];
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+    private Group tileGroup = new Group();
+    private Group pieceGroup = new Group();
+
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
+        root.getChildren().addAll(tileGroup, pieceGroup);
+
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Tile tile = new Tile((x + y) % 2 == 0, x, y);
+                board[x][y] = tile;
+
+                tileGroup.getChildren().add(tile);
+
+                Piece piece = null;
+
+                if (y <= 2 && (x + y) % 2 != 0) {
+                    piece = makePiece(PieceType.BLACK, x, y);
+                }
+
+                if (y >= 5 && (x + y) % 2 != 0) {
+                    piece = makePiece(PieceType.WHITE, x, y);
+                }
+
+                if (piece != null) {
+                    tile.setPiece(piece);
+                    pieceGroup.getChildren().add(piece);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(createContent());
+        primaryStage.setTitle("CheckersApp");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private Piece makePiece(PieceType type, int x, int y) {
+        Piece piece = new Piece(type, x, y);
+        return piece;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,20 +1,23 @@
-/** */
 module com.pk {
-  requires transitive javafx.controls;
-  requires javafx.fxml;
-  requires static lombok;
-  requires org.slf4j;
-  requires java.sql;
-
-  opens com.pk.lanserver;
-  opens com.pk;
-  opens com.pk.frontend.menu;
-
-  exports com.pk;
-  exports com.pk.logic;
-  exports com.pk.logic.exceptions;
-  exports com.pk.lanserver;
-  exports com.pk.lanserver.exceptions;
-  exports com.pk.lanserver.models;
-  exports com.pk.frontend.menu;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.media;
+    requires transitive javafx.controls;
+    requires static lombok;
+    requires org.slf4j;
+    requires java.sql;
+  
+    opens com.pk.lanserver;
+    opens com.pk;
+    opens com.pk.frontend.menu;
+  
+    exports com.pk;
+    exports com.pk.logic;
+    exports com.pk.logic.exceptions;
+    exports com.pk.lanserver;
+    exports com.pk.lanserver.exceptions;
+    exports com.pk.lanserver.models;
+    exports com.pk.frontend.menu;
 }
