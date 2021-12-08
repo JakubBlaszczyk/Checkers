f4cd216722446b3d8bd4b3960a992589f2a61ce6
gabafior <egzystencjalna.pustka@gmail.com>
2021-12-08 21:55:15 +0100

    add board

---
+++ gabi_checkers/gabi_checkers/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module pk {
+    //requires kotlin.stdlib;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.media;
+    //requires jimObjModelImporterJFX;
+}
