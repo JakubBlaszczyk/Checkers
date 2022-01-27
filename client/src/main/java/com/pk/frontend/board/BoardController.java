14229cca428209a8ee4c4a8f0f881996e94f2126
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2022-01-27 14:10:56 +0100

    Revert "Add row num to improved logic constructor"
    This reverts commit 9650c49820bb79231be36cd821a2dc2f39f98813.

--- client/src/main/java/com/pk/frontend/board/BoardController.java
+++ client/src/main/java/com/pk/frontend/board/BoardController.java
@@ -101,25 +101,25 @@ public class BoardController {
     return piece;
   }
 
-  // private MoveResult tryMove(Piece piece, int newX, int newY) {
-  //   if (board[newX][newY].hasPiece() || (newX + newY) % 2 == 0) {
-  //     return new MoveResult(MoveType.NONE);
-  //   }
+  private MoveResult tryMove(Piece piece, int newX, int newY) {
+    if (board[newX][newY].hasPiece() || (newX + newY) % 2 == 0) {
+      return new MoveResult(MoveType.NONE);
+    }
 
-  //   int x0 = toBoard(piece.getOldX());
-  //   int y0 = toBoard(piece.getOldY());
+    int x0 = toBoard(piece.getOldX());
+    int y0 = toBoard(piece.getOldY());
 
-  //   if (Math.abs(newX - x0) == 1 && newY - y0 == piece.getType().moveDir) {
-  //     return new MoveResult(MoveType.NORMAL);
-  //   } else if (Math.abs(newX - x0) == 2 && newY - y0 == piece.getType().moveDir * 2) {
+    if (Math.abs(newX - x0) == 1 && newY - y0 == piece.getType().moveDir) {
+      return new MoveResult(MoveType.NORMAL);
+    } else if (Math.abs(newX - x0) == 2 && newY - y0 == piece.getType().moveDir * 2) {
 
-  //     int x1 = x0 + (newX - x0) / 2;
-  //     int y1 = y0 + (newY - y0) / 2;
+      int x1 = x0 + (newX - x0) / 2;
+      int y1 = y0 + (newY - y0) / 2;
 
-  //     if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
-  //       return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
-  //     }
-  //   }
+      if (board[x1][y1].hasPiece() && board[x1][y1].getPiece().getType() != piece.getType()) {
+        return new MoveResult(MoveType.KILL, board[x1][y1].getPiece());
+      }
+    }
 
     return new MoveResult(MoveType.NONE);
   }
